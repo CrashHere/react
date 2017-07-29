3ac8f2381bbb880d73c9be619efac3c55ff28d20
daffron <daffron92@gmail.com>
2017-07-30 10:21:05 +1200

    redux setup

--- src/Options/components/GpsOrManual/index.js
+++ src/Options/components/GpsOrManual/index.js
@@ -13,9 +13,6 @@ class GpsOrManual extends Component {
           <span>Dont have a place to sleep tonight?<br/>Give us your location to get help.</span>
         </div>
         <div className="gps-or-manual-buttons">
-          <div className="gps-or-manual-button">
-            <span className="gps-or-manual-button-text">Use GPS</span>
-          </div>
           <div className="gps-or-manual-button" onClick={onOptionClick("location")}>
             <span className="gps-or-manual-button-text">Enter Your Location</span>
           </div>
--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import {Provider} from 'react-redux'
+import {createStore, applyMiddleware, compose} from 'redux'
+import thunkMiddleware from 'redux-thunk'
+import reducers from './reducers'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let store = createStore(reducers, compose(
+  applyMiddleware(thunkMiddleware),
+  window.devToolsExtension ? window.devToolsExtension() : f => f
+))
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+, document.getElementById('root'));
 registerServiceWorker();
